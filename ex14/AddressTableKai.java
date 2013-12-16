82ff31167ce34dc04d7a09af607f2e1a597db866
Hiroto Takahashi <elzzup.htam@i.softbank.jp>
2013-12-17 01:12:39 +0900

    後一個

--- ex14/AddressTableKai.java
+++ ex14/AddressTableKai.java
@@ -1,5 +1,120 @@
 package ex14;
 
-public class AddressTableKai {
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.AbstractTableModel;
+
+public class AddressTableKai extends JFrame {
+
+	public static void main(String[] args) {
+		JFrame frame = new AddressTableKai("プログラミング言語表[");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+
+	public static String file_path = "ex14/data/";
+	public static String file_name = "address.txt";
+
+	AddressTableKai(String title) {
+		super(title);
+		JTable table = new JTable(new Table1Model());
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+//		DefaultTableColumnModel tcm = (DefaultTableColumnModel) table.getColumnModel();
+		JScrollPane scroll = new JScrollPane(table);
+		JPanel pane = (JPanel) this.getContentPane();
+		pane.setLayout(new BorderLayout());
+		pane.add(scroll, BorderLayout.CENTER);
+
+		JPanel headPane = new JPanel (new GridLayout(1, 3));
+		JButton buAdd   = new JButton("行の追加");
+		JButton buRead  = new JButton("読み込み");
+		JButton buWrite = new JButton("書き込み");
+		headPane.add(buAdd);
+		headPane.add(buRead);
+		headPane.add(buWrite);
+
+		pane.add(headPane, BorderLayout.PAGE_START);
+
+		setSize(750, 200);
+		setVisible(true);
+	}
+
+	class AddActionListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+		}
+	}
+
+	class Table1Model extends AbstractTableModel {
+		String[] columnNames = "名前,住所,電話,メール".split(",");
+		ArrayList<String[]> list = new ArrayList<String[]>();
+		public Table1Model() {
+			super();
+			try {
+				@SuppressWarnings("resource")
+				BufferedReader br = new BufferedReader(new FileReader(new File(file_path + file_name)));
+				String line;
+				while ((line = br.readLine()) != null) {
+					String[] words = line.split(",");
+					list.add(words);
+					for (String w: words) System.out.println(w);
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println(file_path + file_name + "が見つかりません。");
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public int getRowCount() {
+			return list.size();
+		}
+
+		@Override
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
+
+		@Override
+		public Object getValueAt(int row, int col) {
+			System.out.println(row + ":" + col);
+			return list.get(row)[col];
+		}
+
+		@Override
+		public Class getColumnClass(int col) {
+			return getValueAt(0, col).getClass();
+		}
+
+		@Override
+		public boolean isCellEditable(int row, int col) {
+			return false;
+		}
+
+		@Override
+		public void setValueAt(Object value, int row, int column) {
+			list.get(row)[column] = (String) value;
+		}
+	}
 
 }
--- ex14/JTableExample.java
+++ ex14/JTableExample.java
@@ -4,63 +4,82 @@ import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableColumnModel;
 
 public class JTableExample extends JFrame {
 
-  public static void main(String[] args) {
-    JFrame frame = new JTableExample( "セリーグ順位表" );
-    frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-  }
+	public static void main(String[] args) {
+		JFrame frame = new JTableExample("プログラミング言語表[");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
-  JTableExample ( String title ) {
-    super( title );
-    JTable table = new JTable( new Table1Model() );
-    JScrollPane scroll = new JScrollPane( table );
-    getContentPane().add( scroll );
-    setSize( 600, 200 );
-    setVisible( true );
-  }
+	JTableExample(String title) {
+		super(title);
+		JTable table = new JTable(new Table1Model());
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
+		DefaultTableColumnModel tcm = (DefaultTableColumnModel)table.getColumnModel();
+		int[] width = {50, 200, 40, 60, 200, 200};
+		for (int i =0; i < width.length; i++) {
+			tcm.getColumn(i).setMinWidth(width[i]);
+		}
+		JScrollPane scroll = new JScrollPane(table);
+		getContentPane().add(scroll);
+		setSize(750, 200);
+		setVisible(true);
+	}
 
-  class Table1Model extends AbstractTableModel {
-    String[] columnNames = {
-      "チーム", "勝数", "負数", "分数", "勝率", "ゲーム差"
-    };
-    String[][] data = {
-      { "阪神",     "11",  "2", "0", ".846", "-" },
-      { "広島",     " 8",  "5", "0", ".615", "3.0" },
-      { "巨人",     " 7",  "6", "0", ".538", "1.0" },
-      { "ヤクルト", " 6",  "8", "0", ".428", "1.5" },
-      { "中日",     " 5",  "8", "0", ".384", "0.5" },
-      { "横浜",     " 3", "11", "0", ".214", "2.5" },
-    };
-    @Override
-    public int getColumnCount() {
-      return columnNames.length;
-    }
-    @Override
-    public int getRowCount() {
-      return data.length;
-    }
-    @Override
-    public String getColumnName( int col ) {
-      return columnNames[col];
-    }
-    @Override
-    public Object getValueAt( int row, int col ) {
-      return data[row][col];
-    }
-    @Override
-    public Class getColumnClass( int col ) {
-      return getValueAt( 0, col ).getClass();
-    }
-    @Override
-    public boolean isCellEditable( int row, int col ) {
-      return false;
-    }
-    @Override
-    public void setValueAt( Object value, int row, int column ) {
-      data[row][column] = (String)value;
-    }
-  }
+	class Table1Model extends AbstractTableModel {
+		String[] columnNames = "名前,パラダイム,登場,型付け,影響を受けた言語,影響を与えた言語".split(",");
+		String[][] data = {
+				"Fortran,手続き・構造化・命令・オブジェクト指向,1957,強い静的,,ALGOL・BASIC・PL/I・C".split(","),
+				"COBOL,構造・命令・オブジェクト指向,1959,強い静的,FLOW-MATIC・COMTRAN,PL/I".split(","),
+				"LISP,関数・手続き,1958,強い動的,,LOGO,Perl,Python,Ruby,C#".split(","),
+				"C言語,手続き,1972,弱い静的,B言語・ALGOL・pascal,C++・Objectiv-c・D言語・Java".split(","),
+				"Ruby,構造、命令、オブジェクト指向,1995,強い動的,Smalltalk・LISP・Perl,Groovy".split(","),
+				"Haskell,関数,1990,強い静的,Miranda,Factor".split(","),
+				"Java,構造・命令・オブジェクト指向,1995,強い静的,Objective-C・Smalltalk・C++・Effel,C#・D".split(","),
+				"PHP,命令・オブジェクト指向,1995,弱い動的,C・C++・Java・Perl,_".split(","),
+		};
+
+		public Table1Model() {
+			super();
+		}
+
+		@Override
+		public int getColumnCount() {
+			return columnNames.length;
+		}
+
+		@Override
+		public int getRowCount() {
+			return data.length;
+		}
+
+		@Override
+		public String getColumnName(int col) {
+			return columnNames[col];
+		}
+
+		@Override
+		public Object getValueAt(int row, int col) {
+			System.out.println(row + ":" + col);
+			return data[row][col];
+		}
+
+		@Override
+		public Class getColumnClass(int col) {
+			return getValueAt(0, col).getClass();
+		}
+
+		@Override
+		public boolean isCellEditable(int row, int col) {
+			return false;
+		}
+
+		@Override
+		public void setValueAt(Object value, int row, int column) {
+			data[row][column] = (String) value;
+		}
+	}
 
 }
